ebcc205684e631d0c78710d8403a068e67282805
FANOOJungjieun <jbh1657@naver.com>
2022-05-16 16:29:30 +0900

    querystring

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 var http = require('http');
 var fs = require('fs');
+//var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    const myURL = new URL('http://localhost:3000'+_url);
+    var queryData = myURL.searchParams.get('id');
+    
+    if(queryData) {
+    } else {
+    queryData = undefined;
     }
-    if(request.url == '/favicon.ico'){
+    console.log(queryData);
+
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    //response.end(fs.readFileSync(__dirname + _url)); //기존 홈페이지를 출력하는 코드
+    response.end(queryData); //추출한 쿼리스트링을 브라우저에 출력하는 코드
  
 });
 app.listen(3000);
---
+++ syntax/string.js
@@ -0,0 +1,16 @@
+console.log('1'+'1');
+console.log('asddddddddddddddd dwwewwqkkkkkkkkkkkkkkkkkkkkk nomeaning sentancesssssssssssssssssssssss'.length)
+
+var str = 'asddddddddddddddd dwwewwqkkkkkkkkkkkkkkkkkkkkk nomeaning sentancesssssssssssssssssssssss';
+console.log(str.length);
+
+var letter = 'Dear \n\n\
+\
+this is letter';
+
+var letter2 = `Dear
+
+this is letter`;
+
+console.log(letter);
+console.log(letter2);
