42d6f4370fc0490041f1801ede149e386d0af4d3
FANOOJungjieun <jbh1657@naver.com>
2022-05-16 15:36:52 +0900

    .

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
-
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = new URL('http://localhost:3000' + _url).searchParams;
-
-    console.log(queryData.get('id'));
-    if(_url == '/'){
-      _url = '/index.html';
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
     }
-    if(_url == '/favicon.ico'){
+    if(request.url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
+    response.end(fs.readFileSync(__dirname + url));
  
 });
 app.listen(3000);
