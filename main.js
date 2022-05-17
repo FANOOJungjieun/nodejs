d425e04bf4e0d5cc347cede5d272994ed2e09d64
FANOOJungjieun <jbh1657@naver.com>
2022-05-17 13:15:54 +0900

    예외처리

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     const myURL = new URL('http://localhost:3000'+_url);
     var queryData = myURL.searchParams.get('id');
-    var title = queryData;
+    var pathname = myURL.pathname;
     
     if(queryData) {
     } else {
@@ -14,41 +14,66 @@ var app = http.createServer(function(request,response){
     }
     console.log(queryData);
 
-    if(_url == '/'){
-      //_url = '/index.html'; //정적 코드
-      title='Welcome';
+    if(pathname === '/') {
+      if(queryData === undefined) {
+        fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
+          var title = 'Welcome';
+          var data = 'Hello, NodeJS';
+          var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>${data}
+      </p>
+    </body>
+    </html>
+        `;
+        response.writeHead(200);
+        response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
+        });
+      } else {
+        fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
+          var title = queryData;
+          var template = `
+        <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>${data}
+      </p>
+    </body>
+    </html>
+        `;
+        response.writeHead(200);
+        response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not Found');
     }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
     //response.end(fs.readFileSync(__dirname + _url)); //기존 정적인 홈페이지를 출력하는 코드
-
-    fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
-      var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${data}
-  </p>
-</body>
-</html>
-    `;
-    response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
-    });
  
 });
 app.listen(3000);
