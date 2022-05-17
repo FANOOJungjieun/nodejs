d534191418891efd8708460417134c6a708b6253
FANOOJungjieun <jbh1657@naver.com>
2022-05-17 15:22:04 +0900

    readdir

--- main.js
+++ main.js
@@ -12,62 +12,76 @@ var app = http.createServer(function(request,response){
     } else {
     queryData = undefined;
     }
-    console.log(queryData);
 
     if(pathname === '/') {
       if(queryData === undefined) {
-        fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
+
+        fs.readdir('./data', function(err, filelist) {
+          console.log(filelist);
           var title = 'Welcome';
           var data = 'Hello, NodeJS';
+
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length) {
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+              i = i+1;
+          }
+          var list = list + '</ul>';
+
           var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${data}
-      </p>
-    </body>
-    </html>
-        `;
+                  <!doctype html>
+              <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${data}
+                </p>
+              </body>
+              </html>
+                  `;
         response.writeHead(200);
-        response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
-        });
+        response.end(template); 
+        })
+          
       } else {
-        fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
-          var title = queryData;
-          var template = `
-        <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${data}
-      </p>
-    </body>
-    </html>
-        `;
-        response.writeHead(200);
-        response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
-        });
+        fs.readdir('./data', function(err, filelist) {
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length) {
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+              i = i+1;
+          }
+          var list = list + '</ul>';
+
+          fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
+            var title = queryData;
+            var template = `
+                    <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  ${list}
+                  <h2>${title}</h2>
+                  <p>${data}
+                  </p>
+                </body>
+                </html>
+                    `;
+          response.writeHead(200);
+          response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
+          });
+
+      })
       }
     } else {
       response.writeHead(404);
