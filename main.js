a654e09b15c96b0c756b71434286bc47ca239d81
FANOOJungjieun <jbh1657@naver.com>
2022-05-24 16:53:54 +0900

    글 삭제

--- main.js
+++ main.js
@@ -62,7 +62,14 @@ var app = http.createServer(function(request,response){
 
           fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
             var title = queryData;
-            var template = templateHTML(title,list,`<h2>${title}</h2>${data}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            var template = templateHTML(title,list,`<h2>${title}</h2>${data}`, 
+            `<a href="/create">create</a> 
+             <a href="/update?id=${title}">update</a>
+             <form action="delete_process" method="post" onsubmit="return confirm('do you want to delete this file?')">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+             </form>
+             `);
 
             response.writeHead(200);
             response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
@@ -146,6 +153,22 @@ var app = http.createServer(function(request,response){
               response.end();
             })
           })
+      })
+    } else if(pathname==='/delete_process') {
+      var body = '';
+      request.on('data', function(data) {
+          body = body + data;
+      })
+      request.on('end', function() {
+          var post = qs.parse(body);
+
+          var id = post.id;
+
+          fs.unlink(`data/${id}`, function(err) {
+            response.writeHead(302, {location: `/`});
+            response.end();
+          })
+
       })
     } else {
       response.writeHead(404);
