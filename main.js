fa20bbf6981fa3f86a88f4ca35c5da7b7462fcb2
FANOOJungjieun <jbh1657@naver.com>
2022-05-24 16:11:31 +0900

    글 수정,전송

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var qs = require('querystring');
 //var url = require('url');
 
-function templateHTML(title,list,data) {
+function templateHTML(title,list,data,control) {
   return `
       <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title,list,data) {
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${data}
     </body>
     </html>
@@ -51,7 +51,7 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var data = 'Hello, NodeJS';
           var list = templateList(filelist);
-          var template = templateHTML(title,list,`<h2>${title}</h2>${data}`);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${data}`, `<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template); 
         })
@@ -62,7 +62,7 @@ var app = http.createServer(function(request,response){
 
           fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
             var title = queryData;
-            var template = templateHTML(title,list,`<h2>${title}</h2>${data}`);
+            var template = templateHTML(title,list,`<h2>${title}</h2>${data}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
 
             response.writeHead(200);
             response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
@@ -75,7 +75,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB-create';
         var list = templateList(filelist);
         var template = templateHTML(title,list,`
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -84,7 +84,7 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
         </form>
-        `);
+        `, ``);
         response.writeHead(200);
         response.end(template); 
       })
@@ -104,6 +104,30 @@ var app = http.createServer(function(request,response){
             response.end();
           })
       })
+    } else if(pathname=== '/update') {
+      fs.readdir('./data', function(err, filelist) {
+        var list = templateList(filelist);
+
+        fs.readFile(`data/${queryData}`, 'utf8', function(err,data){
+          var title = queryData;
+          var template = templateHTML(title,list,`
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${data}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+
+          response.writeHead(200);
+          response.end(template); //추출한 쿼리스트링을 브라우저에 출력하는 코드
+        });
+
+    })
     } else {
       response.writeHead(404);
       response.end('Not Found');
