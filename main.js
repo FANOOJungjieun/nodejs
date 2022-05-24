e265c46e836dfeb778eae54e3647f60b92baf084
FANOOJungjieun <jbh1657@naver.com>
2022-05-24 16:19:00 +0900

    수정사항 update

--- main.js
+++ main.js
@@ -128,6 +128,25 @@ var app = http.createServer(function(request,response){
         });
 
     })
+    } else if(pathname==='/update_process') {
+      var body = '';
+      request.on('data', function(data) {
+          body = body + data;
+      })
+      request.on('end', function() {
+          var post = qs.parse(body);
+
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+
+          fs.rename(`data/${id}`, `data/${title}`, function(err) {
+            fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
+              response.writeHead(302, {location: `/?id=${title}`});
+              response.end();
+            })
+          })
+      })
     } else {
       response.writeHead(404);
       response.end('Not Found');
